4cae2aec58c2071967326a3a3fa4744bd5899b1f
N.Shimizu <chikoski@gmail.com>
2024-01-21 18:15:35 +0900

    Fix for doc-test errors

--- crates/png-glitch/src/lib.rs
+++ crates/png-glitch/src/lib.rs
@@ -1,12 +1,10 @@
-extern crate core;
-
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
 
-pub use crate::png::{FilterType, GlitchContext, ScanLine};
 use crate::png::Encoder;
 use crate::png::Png;
+pub use crate::png::{FilterType, GlitchContext, ScanLine};
 
 mod png;
 
@@ -15,16 +13,20 @@ mod png;
 ///
 /// # Examples
 ///
-/// The following snippet shows how you can glitch "./a_png_file.png" and save the generated image as "./glitched.png".
+/// The following snippet shows how you can glitch "./etc/sample00.png" and save the generated image as "./glitched.png".
 ///
 /// ```
-/// use png_glitch::{FilterType, PngGlitch};
-/// let mut png_glitch = PngGlitch::open("./a_png_file.png")?;
+/// # use std::env;
+/// # env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string())).expect("");
+///
+/// use png_glitch::{FilterType, PngGlitch};///
+///
+/// let mut png_glitch = PngGlitch::open("./etc/sample00.png").expect("The PNG file should be successfully parsed");
 /// png_glitch.foreach_scanline(|scan_line|{
 ///   scan_line.set_filter_type(FilterType::None);
 ///   scan_line[4] = 1;
 /// });
-/// png_glitch.save("./glitched.png")?;
+/// png_glitch.save("./glitched.png").expect("The glitched file should be saved as a PNG file");
 /// ```
 ///
 pub struct PngGlitch {
@@ -37,8 +39,12 @@ impl PngGlitch {
     /// # Example
     ///
     /// ```
+    /// # use std::env;
+    /// # env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string())).expect("");
+    ///
     /// use png_glitch::PngGlitch;
-    /// let mut png_glitch = PngGlitch::open("./a_png_file.png")?;
+    ///
+    /// let mut png_glitch = PngGlitch::open("./etc/sample00.png").expect("The PNG file should be successfully parsed");
     /// ```
     pub fn open(path: impl AsRef<Path>) -> anyhow::Result<PngGlitch> {
         let mut file = File::open(path)?;
@@ -57,10 +63,11 @@ impl PngGlitch {
     /// use std::fs::File;
     /// use std::io::Read;
     /// use png_glitch::PngGlitch;
+    ///
     /// let mut buffer = vec![];
-    /// let mut file = File::open("./a_png_file/png")?;
-    /// file.read_to_end(&mut buffer)?;
-    /// let mut png_glitch = PngGlitch::new(buffer)?;
+    /// let mut file = File::open("./etc/sample00.png").expect("The file should be opened");
+    /// file.read_to_end(&mut buffer).expect("The bytes in the file should be written into the buffer");
+    /// let mut png_glitch = PngGlitch::new(buffer).expect("The data in the buffer should be successfully parsed as PNG");
     /// ```
     pub fn new(buffer: Vec<u8>) -> anyhow::Result<PngGlitch> {
         let png = Png::try_from(&buffer)?;
@@ -73,17 +80,24 @@ impl PngGlitch {
     /// # Example
     ///
     /// ```
+    /// # use std::env;
+    /// # env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string())).expect("");
+    ///
     /// use png_glitch::PngGlitch;
-    /// let mut png_glitch = PngGlitch::open("./a_png_file.png")?;
+    ///
+    /// let mut png_glitch = PngGlitch::open("./etc/sample00.png").expect("The PNG file should be successfully parsed");
     /// png_glitch.glitch(|context|{
     ///   let mut index = 1;
-    ///   while index < context.width() {
+    ///   while index  < (context.width() as usize) {
     ///      context.data()[index] = 0;
     ///      index += context.scan_line_width();
     ///   }
     /// });
     /// ```
-    pub fn glitch<F>(&mut self, modifier: F) where F: FnMut(&mut GlitchContext) {
+    pub fn glitch<F>(&mut self, modifier: F)
+    where
+        F: FnMut(&mut GlitchContext),
+    {
         self.png.glitch(modifier)
     }
 
@@ -95,13 +109,20 @@ impl PngGlitch {
     /// The following example changes the filter method of all scan line to None.
     ///
     /// ```
+    /// # use std::env;
+    /// # env::set_current_dir(env::var("CARGO_MANIFEST_DIR").unwrap_or(".".to_string())).expect("");
+    ///
     /// use png_glitch::{FilterType, PngGlitch};
-    /// let mut png_glitch = PngGlitch::open("./a_png_file.png")?;
+    ///
+    /// let mut png_glitch = PngGlitch::open("./etc/sample00.png").expect("The PNG file should be successfully parsed");
     /// png_glitch.foreach_scanline(|scan_line|{
     ///    scan_line.set_filter_type(FilterType::None);
     /// });
     /// ```
-    pub fn foreach_scanline<F>(&mut self, modifier: F) where F: FnMut(&mut ScanLine) {
+    pub fn foreach_scanline<F>(&mut self, modifier: F)
+    where
+        F: FnMut(&mut ScanLine),
+    {
         self.png.foreach_scanline(modifier)
     }
 
@@ -109,11 +130,12 @@ impl PngGlitch {
     ///
     /// # Example
     ///
-    /// The following example copies `./a_png_file.png` as `./another_png_file.png`.
+    /// The following example copies `./etc/sample00.png` as `./glitched.png`.
     /// ```
     /// use png_glitch::{FilterType, PngGlitch};
-    /// let png_glitch = PngGlitch::open("./a_png_file.png")?;
-    /// png_glitch.save("./another_png_file.png")?;
+    ///
+    /// let png_glitch = PngGlitch::open("etc/sample00.png").expect("The PNG file should be successfully parsed");
+    /// png_glitch.save("./glitched.png").expect("The glitched PNG data should be saved to the given path");
     /// ```
     pub fn save(&self, path: impl AsRef<Path>) -> anyhow::Result<()> {
         self.png.save(path)
@@ -127,11 +149,12 @@ impl PngGlitch {
     ///
     /// ```
     /// use png_glitch::PngGlitch;
-    /// let png_glitch = PngGlitch::open("./a_png_file.png")?;
-    /// let mut encoded_data = vec![];
-    /// png_glitch.encode(&mut encoded_data)?;
+    ///
+    /// let png_glitch = PngGlitch::open("./etc/sample00.png").expect("The PNG file should be successfully parsed");
+    /// let mut encoded_data:Vec<u8> = vec![];
+    /// png_glitch.encode(&mut encoded_data).expect("The glitched PNG data should be written into the encoded_data in PNG format");
     /// ```
-    pub fn encode(&self, buffer: &mut [u8]) -> anyhow::Result<()> {
+    pub fn encode(&self, buffer: &mut Vec<u8>) -> anyhow::Result<()> {
         let _ = self.png.encode(buffer)?;
         Ok(())
     }
