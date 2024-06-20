f8e19bee97396f56ba49ff49d6c0a76573764b8b
N.Shimizu <chikoski@gmail.com>
2024-06-20 21:59:48 +0900

    refactor codes

--- src/cli.rs
+++ src/cli.rs
@@ -6,3 +6,17 @@ pub struct Cli {
     pub output_file: Option<String>,
     pub png_file: String,
 }
+
+pub struct Config {
+    pub output_file: String,
+    pub input_file: String,
+}
+
+impl From<Cli> for Config {
+    fn from(value: Cli) -> Self {
+        Config {
+            output_file: value.output_file.unwrap(),
+            input_file: value.png_file
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -4,29 +4,30 @@ use clap::Parser;
 
 use png_glitch::PngGlitch;
 
-use crate::cli::Cli;
+use crate::cli::{Cli, Config};
 
 mod cli;
 
 fn main() {
-    let arguments = Cli::parse();
-    let input_file = arguments.png_file;
-    let output_file = arguments.output_file.unwrap();
+    let config = Cli::parse().into();
 
-    if let Err(e) = run(input_file, output_file) {
+    if let Err(e) = start(&config) {
         println!("{:?}", e);
     }
 }
 
-fn run(input: impl AsRef<Path>, output: impl AsRef<Path>) -> anyhow::Result<()> {
-    let mut glitch = PngGlitch::open(input)?;
+fn start(config: &Config) -> anyhow::Result<()> {
+    let mut glitch = PngGlitch::open(&config.input_file)?;
+    run(&mut glitch);
+    glitch.save(&config.output_file)?;
+    Ok(())
+}
 
+fn run(glitch: &mut PngGlitch)  {
     glitch.glitch(|context| {
         context.data()[1] = 0;
     });
     glitch.foreach_scanline(|scanline| {
         scanline[2] = 0;
     });
-
-    glitch.save(output)
 }
