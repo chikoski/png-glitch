bdc334958de7a376dc59d007d87e32f5f53c7e33
N.Shimizu <chikoski@gmail.com>
2024-07-01 08:28:59 +0900

    Implement Read::read_to_end for ScanLine;

--- crates/png-glitch/src/png/scan_line.rs
+++ crates/png-glitch/src/png/scan_line.rs
@@ -38,6 +38,11 @@ impl<'a> Read for ScanLine<'a> {
         let mut buffer = &self.inner[1..];
         buffer.read(buf)
     }
+
+    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> std::io::Result<usize> {
+        let mut buffer = &self.inner[1..];
+        buffer.read_to_end(buf)
+    }
 }
 
 impl<'a> Write for ScanLine<'a> {
@@ -73,5 +78,75 @@ impl<'a> TryFrom<&'a mut [u8]> for ScanLine<'a> {
     fn try_from(value: &'a mut [u8]) -> Result<Self, Self::Error> {
         let filter_type = FilterType::try_from(value[0])?;
         Ok(ScanLine::new(filter_type, value))
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    struct TestTarget {
+        buffer: Vec<u8>,
+    }
+
+    impl<'a> TestTarget {
+        fn new() -> Self {
+            let buffer = vec![0, 1, 2, 3, 4, 5];
+            TestTarget {
+                buffer,
+            }
+        }
+
+        fn scan_line(&'a mut self) -> ScanLine<'a> {
+            ScanLine::new(FilterType::None, &mut self.buffer)
+        }
+    }
+
+    mod read {
+        use std::io::Read;
+        use super::*;
+
+        #[test]
+        fn test_read() {
+            let mut target = TestTarget::new();
+            let mut scan_line = target.scan_line();
+
+            let mut buffer = vec![0; scan_line.size()];
+
+            let result = scan_line.read(&mut buffer);
+            assert_eq!(true, result.is_ok());
+            assert_eq!(scan_line.size(), buffer.len());
+            assert_eq!(&scan_line.inner[1..], &buffer);
+        }
+
+        #[test]
+        fn test_read_to_end() {
+            let mut target = TestTarget::new();
+            let mut scan_line = target.scan_line();
+
+            let mut buffer = vec![];
+
+            let size = scan_line.size();
+            let result = scan_line.read_to_end(&mut buffer);
+            assert_eq!(true, result.is_ok());
+            assert_eq!(&scan_line.inner[1..], &buffer[0..size]);
+        }
+    }
+
+    mod write {
+        use super::*;
+
+        #[test]
+        fn test_write() {
+            let mut taget = TestTarget::new();
+            let mut scan_line = taget.scan_line();
+            let size = scan_line.size();
+
+            let buffer = vec![10; size];
+            let result = scan_line.write(&buffer);
+            assert_eq!(true, result.is_ok());
+            assert_eq!(buffer.len(), result.unwrap());
+            assert_eq!(&buffer, &scan_line.inner[1..]);
+        }
     }
 }
